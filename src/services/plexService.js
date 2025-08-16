445966329c985093bbdbf0643f0415a21523f4e3
Pelle Härdstedt <pelle.hardstedt@gmail.com>
2025-08-16 21:53:21 +0200

    feat: add configuration check to triggerScan function to control Plex scanning

--- src/services/plexService.js
+++ src/services/plexService.js
@@ -6,8 +6,14 @@ const PLEX_TOKEN = process.env.PLEX_TOKEN;
 const PLEX_HOST = process.env.PLEX_HOST || 'localhost';
 const PLEX_PORT = process.env.PLEX_PORT || 32400;
 const PLEX_LIBRARY_PATH = process.env.PLEX_LIBRARY_PATH;
+const ENABLE_PLEX_SCAN = process.env.ENABLE_PLEX_SCAN === 'true';
 
 async function triggerScan() {
+  if (!ENABLE_PLEX_SCAN) {
+    console.log('Plex scan skipped due to configuration.');
+    return;
+  }
+
   const urls = [
     `http://${PLEX_HOST}:${PLEX_PORT}/library/sections/1/refresh?X-Plex-Token=${PLEX_TOKEN}`,
     `http://${PLEX_HOST}:${PLEX_PORT}/library/sections/2/refresh?X-Plex-Token=${PLEX_TOKEN}`
